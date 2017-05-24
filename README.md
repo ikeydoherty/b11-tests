94942452697b1553f8188b16516846bab8ab3e48
Ikey Doherty <ikey@solus-project.com>
2017-05-24 21:06:24 +0100

    Initial commit
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+b11-tests
+---------
+
+Casual hacking on Qt bits for upcoming B11 work.
---
+++ src/b11-panel.cpp
@@ -0,0 +1,21 @@
+/*
+ * This file is part of b11-tests
+ *
+ * Copyright © 2015-2017 Ikey Doherty <ikey@solus-project.com>
+ *
+ * b11-tests is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#include <QApplication>
+#include <QWindow>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QWindow window;
+    window.show();
+    return app.exec();
+}
